9831e513e5ea666cf88d36ab3b46c80d769f26f7
Igor Moura Sousa <igormsousa2003@gmail.com>
2023-10-03 08:51:13 -0300

    adicionando dados

---
+++ src/app/data/data.ts
@@ -0,0 +1,299 @@
+export const data = [
+    {
+        "area": [
+
+            {
+                "id": "1",
+                "area": "Front - end",
+                "bgSrc": "../../../assets/bg-area/bg-frontend.jpg",
+                "bgAlt": "Fundo Front - End",
+                "style": "width: 120px; height: 170px; border: 2px solid #3E68FF;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": "Front End é responsável por desenvolver por meio do código uma interface gráfica"
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Tecnologias",
+                        "paragrafo": "normalmente, com as tecnologias base da Web: HTML, CSS e JavaScript"
+                    }
+                ]
+            },
+            {
+                "id": "2",
+                "area": "Back - end",
+                "bgSrc": "../../../assets/bg-area/bg-backend.jpg",
+                "bgAlt": "Fundo Back - End",
+                "style": "width: 120px; height: 170px; border: 2px solid #3DF850;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Tecnologias",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "3",
+                "area": "Full - end",
+                "bgSrc": "../../../assets/bg-area/bg-fullstack.jpg",
+                "bgAlt": "Fundo Full - Stack",
+                "style": "width: 120px; height: 170px; border: 2px solid #FF3860;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Tecnologias",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        "tecnologia": [
+
+            {
+                "id": "1",
+                "tecnologia": "Internet",
+                "icon": "fa-solid fa-globe",
+                "style": "color: #0FB6FA; width: 90px; height: 90px; border: 2px solid #0FB6FA;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "2",
+                "tecnologia": "HTML",
+                "icon": "fa-brands fa-html5",
+                "style": "color: #E5532D; width: 90px; height: 90px; border: 2px solid #E5532D;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "3",
+                "tecnologia": "CSS",
+                "icon": "fa-brands fa-css3-alt",
+                "style": "color: #306AF1; width: 90px; height: 90px; border: 2px solid #306AF1;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "4",
+                "tecnologia": "JavaScript",
+                "icon": "fa-brands fa-js",
+                "style": "color: #F7E025; width: 90px; height: 90px; border: 2px solid #F7E025;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "5",
+                "tecnologia": "Git",
+                "icon": "fa-brands fa-git-alt",
+                "style": "color: #F05539; width: 90px; height: 90px; border: 2px solid #F05539;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "6",
+                "tecnologia": "GitHub",
+                "icon": "fa-brands fa-github",
+                "style": "color: #2C3036; width: 90px; height: 90px; border: 2px solid #2C3036;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "7",
+                "tecnologia": "npm",
+                "icon": "fa-brands fa-npm",
+                "style": "color: #CD3E3D; width: 90px; height: 90px; border: 2px solid #CD3E3D;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "8",
+                "tecnologia": "Angular",
+                "icon": "fa-brands fa-angular",
+                "style": "color: #DE0837; width: 90px; height: 90px; border: 2px solid #DE0837;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "9",
+                "tecnologia": "MySQL",
+                "style": "color: #CD3E3D; width: 90px; height: 90px; border: 2px solid #CD3E3D;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "10",
+                "tecnologia": "Node JS",
+                "icon": "fa-brands fa-node-js",
+                "style": "color: #93C745; width: 90px; height: 90px; border: 2px solid #93C745;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+            {
+                "id": "11",
+                "tecnologia": "More",
+                "link": {
+                    "frontend": "https://roadmap.sh/frontend",
+                    "backend": "https://roadmap.sh/backend",
+                    "fullstack": "https://roadmap.sh/full-stack"
+                },
+                "style": "color: #AF04FF; width: 90px; height: 90px; border: 2px solid #AF04FF;",
+                "conteudo":[
+                    {
+                        "titulo": "O que é?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "O que faz ?",
+                        "paragrafo": ""
+                    },
+                    {
+                        "titulo": "Basic Learning",
+                        "paragrafo": ""
+                    }
+                ]
+            },
+        ]
+    }
+
+]
--- src/app/pages/area-tela/area-tela.component.ts
+++ src/app/pages/area-tela/area-tela.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { data } from '../../data/data'
 
 @Component({
   selector: 'app-area-tela',
@@ -6,10 +8,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./area-tela.component.css']
 })
 export class AreaTelaComponent implements OnInit {
+  tituloArea:string = ""
+  private id: string | null = "0"
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id: string | null ) {
+    const result = data.filter(article => console.log(article.area))
+    
+
+  }
+
 
 }
